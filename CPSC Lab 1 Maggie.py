e0646af1254d95d69f1eadbd72a9889e2734c007
Cody Brown <116841702+codynb@users.noreply.github.com>
2022-11-03 15:09:39 -0400

    Add files via upload

---
+++ CPSC Lab 1 Maggie.py
@@ -0,0 +1,69 @@
+Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello, World")
+Hello, World
+print("Hello\nWorld")
+Hello
+World
+print("Hello/nWorld")
+Hello/nWorld
+print("Today", "is", "Tuesday")
+Today is Tuesday
+print("Today" "is" "Tuesday")
+TodayisTuesday
+print("a\nb\nc")
+a
+b
+c
+print("Hello" + "World")
+HelloWorld
+print("Hello\n" + "\nWorld")
+Hello
+
+World
+print("123\n45\n6")
+123
+45
+6
+22/4
+5.5
+22//4
+5
+22%4
+2
+3**2
+9
+2**3
+8
+10%2
+0
+10%3
+1
+10%4
+2
+10%5
+0
+10%6
+4
+10%7
+3
+10%8
+2
+10%9
+1
+10%10
+0
+(4+3)//2
+3
+2(3**4)+1
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    2(3**4)+1
+TypeError: 'int' object is not callable
+2*(3**4)+1
+163
+print("2+2=4")
+2+2=4
+print("50 pounds is" ,50*.453592,"Kilograms")
+50 pounds is 22.6796 Kilograms
+#when finding the product of an equation you need to put the multiplication symbol in front of the parenthesis
---
+++ CSPC Lab 1 Cody.py
@@ -0,0 +1,44 @@
+Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello, world")
+Hello, world
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+#cody brown
+print("How\nare\you?")
+How
+are\you?
+print("How\nare\nyou?")
+How
+are
+you?
+print("Today" + "is" + "Tuesday")
+TodayisTuesday
+print("Today"+ "is"+ "Tuesday")
+TodayisTuesday
+print("Today"+"is"+"Tuesday")
+1+2*3
+SyntaxError: multiple statements found while compiling a single statement
+1+2*3
+7
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+10%1
+0
+2%1
+0
+6%2
+0
+2*(3**4)+1
+163
+print("2+2=", 2+2)
+2+2= 4
+print("50 pounds is", 50*0.453592, "Kilograms")
+50 pounds is 22.6796 Kilograms
+#cody brown
